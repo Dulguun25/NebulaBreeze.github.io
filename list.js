6f3ae4453981a10995068dd3177b9fb4675c6935
Dulguun25 <145865838+Dulguun25@users.noreply.github.com>
2024-02-16 19:09:59 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,27 @@
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const courseElements = document.querySelectorAll(".container-slider .course");
+        const courseElementsAll = document.querySelectorAll(".courses-all .course"); 
+        
+        data.courses.course.forEach((course, index) => {
+            updateCourseElement(courseElements[index], course);
+        });
+
+        data['courses-all'].course.forEach((course, index) => {
+            updateCourseElement(courseElementsAll[index], course); 
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+function updateCourseElement(element, course) {
+    element.querySelector(".title").innerText = course.title;
+    element.querySelector(".teacherName").innerText = course.teacherName;
+    element.querySelector(".score span").innerText = course.score;
+    element.querySelector(".seen span").innerText = course.seen;
+    element.querySelector(".level").innerText = course.level;
+    element.querySelector(".time").innerText = course.time;
+    element.querySelector(".lessons").innerText = course.lessons;
+    element.querySelector(".price").innerText = course.price;
+    element.querySelector(".price-before").innerText = course['price-before'];
+}
